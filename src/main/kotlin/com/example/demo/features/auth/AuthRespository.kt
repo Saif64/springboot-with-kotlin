f24b39622b5b23bc4ccb493974c894f75ab7eae5
saif64s <my@wo.rk>
2025-08-01 21:29:41 +0600

    Add auth repository

--- src/main/kotlin/com/example/demo/features/auth/AuthRespository.kt
+++ src/main/kotlin/com/example/demo/features/auth/AuthRespository.kt
@@ -1,8 +1,10 @@
 package com.example.demo.features.auth
 
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 import java.util.UUID
 
+@Repository
 interface AuthRespository : MongoRepository<User, UUID> {
     fun findByUsername(username: String): User?
 }
