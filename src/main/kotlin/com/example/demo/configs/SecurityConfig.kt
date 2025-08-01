1a555b1aa1c570b6efa03a4b147858adabc24ae5
saif64s <my@wo.rk>
2025-08-01 21:19:42 +0600

    Add basic auth from config file

--- src/main/kotlin/com/example/demo/configs/SecurityConfig.kt
+++ src/main/kotlin/com/example/demo/configs/SecurityConfig.kt
@@ -1,16 +1,48 @@
 package com.example.demo.configs
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
-    fun filterChain(httpSecurity: HttpSecurity) : SecurityFilterChain {
+
+    @Bean
+    fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
         return httpSecurity
             .csrf { it.disable() }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { auth ->
+                auth.requestMatchers("/public/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .httpBasic(Customizer.withDefaults())
             .build()
     }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun userDetailsService(passwordEncoder: PasswordEncoder): UserDetailsService {
+        val user = User.withUsername("test")
+            .password(passwordEncoder.encode("1234")) // IMPORTANT: Encode the password
+            .roles("USER")
+            .build()
+
+        return InMemoryUserDetailsManager(user)
+    }
+
 }
